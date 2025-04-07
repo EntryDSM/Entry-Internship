1923518b09f6cdfc84488f8dc6eb871536673895
minsu0707 <mandoo4137@gmail.com>
2025-04-07 19:01:29 +0900

    feature : 공고 삭제api

--- apps/entry-admin/src/apis/index.ts
+++ apps/entry-admin/src/apis/index.ts
@@ -1,5 +1,5 @@
-export * from './posts';
 export * from './reports';
 export * from './useEditSupport';
 export * from './postAllApi';
 export * from './postDetailApi';
+export * from './postDelete';
